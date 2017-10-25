ed9ab3af54ee9be3cbd90ac3519708cfcb0fdc83
Felipe Antonio Cardoso <felipe.cardoso@nextel.com.br>
2017-10-25 14:14:38 -0200

    added scheme

--- lib/fastlane/plugin/bluepill/actions/bluepill_action.rb
+++ lib/fastlane/plugin/bluepill/actions/bluepill_action.rb
@@ -4,7 +4,8 @@ module Fastlane
       def self.run(params)
         UI.message("Start test by using Bluepill: https://github.com/linkedin/bluepill")
         cmd =  bin_bluepill.to_s
-        cmd << " --xctestrun-path #{params[:xctestrun]}"
+        cmd << " #{params[:xctestrun]}"
+        cmd << " -s #{params[:scheme]}"
         cmd << " -a #{params[:app]}"
         cmd << " -o #{params[:output_dir]}"
         cmd << " -d \"#{params[:device]}\""
